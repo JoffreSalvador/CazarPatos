d200d6474b2e2a98c32d6635d8690a12def199c4
Joffre Salvador <joffre.salvador@epn.edu.ec>
2025-07-18 10:30:35 -0500

    Se modificó el Hall of Fame involucrando Firestore

--- app/src/main/java/com/joffre/salvador/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/joffre/salvador/cazarpatos/RankingActivity.kt
@@ -1,12 +1,17 @@
 package com.joffre.salvador.cazarpatos
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.joffre.salvador.cazarpatos.database.RankingPlayerDBHelper
 
 class RankingActivity : AppCompatActivity() {
@@ -15,27 +20,54 @@ class RankingActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_ranking)
 
-        OperacionesSqLite()
-        GrabarRankingSQLite()
-        LeerRankingsSQLite()
+//        OperacionesSqLite()
+//        GrabarRankingSQLite()
+//        LeerRankingsSQLite()
+//
+//        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }*/
+//        var jugadores = arrayListOf<Player>()
+//        jugadores.add(Player("Joffre.Salvador",10))
+//        jugadores.add(Player("Jugador2",6))
+//        jugadores.add(Player("Jugador3",3))
+//        jugadores.add(Player("Jugador4",9))
+//        jugadores.sortByDescending { it.huntedDucks }
+//
+//        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+//        recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+//        recyclerViewRanking.adapter = RankingAdapter(jugadores);
+//        recyclerViewRanking.setHasFixedSize(true);
+        consultarPuntajeJugadores()
 
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-        var jugadores = arrayListOf<Player>()
-        jugadores.add(Player("Joffre.Salvador",10))
-        jugadores.add(Player("Jugador2",6))
-        jugadores.add(Player("Jugador3",3))
-        jugadores.add(Player("Jugador4",9))
-        jugadores.sortByDescending { it.huntedDucks }
+    }
 
-        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
-        recyclerViewRanking.layoutManager = LinearLayoutManager(this);
-        recyclerViewRanking.adapter = RankingAdapter(jugadores);
-        recyclerViewRanking.setHasFixedSize(true);
-
+    fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("huntedDucks", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Player>()
+                for (document in result) {
+                    val jugador = document.toObject(Player::class.java)
+                    //val jugador = document.toObject<Player>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
     }
 
     fun OperacionesSqLite(){
